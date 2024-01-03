a79e1e5630b1cd70c7ed5fb9e84af79ee219fc49
n7meless <n7meless@gmail.com>
2024-01-03 13:04:48 +0500

    new: init project

---
+++ src/main/kotlin/dev/n7meless/moviespringgraphql/MovieSpringGraphqlApplication.kt
@@ -0,0 +1,11 @@
+package dev.n7meless.moviespringgraphql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MovieSpringGraphqlApplication
+
+fun main(args: Array<String>) {
+    runApplication<MovieSpringGraphqlApplication>(*args)
+}
---
+++ src/test/kotlin/dev/n7meless/moviespringgraphql/MovieSpringGraphqlApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.n7meless.moviespringgraphql
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MovieSpringGraphqlApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
